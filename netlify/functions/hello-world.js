100aeb60c30891cca54f015190c0ed4c20ae191e
Cynthia Teeters <cynthiateeters@gmail.com>
2022-11-18 19:51:24 -0500

    Add chalk and luxon

--- netlify/functions/hello-world.js
+++ netlify/functions/hello-world.js
@@ -1,5 +1,14 @@
 /* eslint-disable no-unused-vars */
-exports.handler = async (event, context) => ({
-  statusCode: 200,
-  body: JSON.stringify({ message: 'Hello World!' }),
-});
+// eslint-disable-next-line no-shadow
+const chalk = require('chalk');
+const { DateTime } = require('luxon');
+
+exports.handler = async (event, context) => {
+  const date = DateTime.now();
+  console.log(chalk.blue(`Hello from Netlify Functions! ${date}`));
+  // console.log(chalk.cyan(`Hello from Netlify Functions! ${date}`));
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ message: 'Hello World!' }),
+  };
+};
--- netlify/functions/pokedex.js
+++ netlify/functions/pokedex.js
@@ -2,6 +2,8 @@
 // mod.cjs
 // eslint-disable-next-line no-shadow
 const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args));
+// eslint-disable-next-line no-shadow
+const chalk = (...args) => import('chalk').then(({ default: chalk }) => chalk(...args));
 
 exports.handler = async (event, context) => {
   const eventBody = JSON.parse(event.body);
